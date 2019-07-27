914e72cc02b43a4b1db38716c9838b2ef5504425
Donald Whitely <dswhitely1@gmail.com>
2019-07-26 22:29:01 -0400

    Redux Store Boilerplated

---
+++ src/store/auth/reducers.js
@@ -0,0 +1,13 @@
+const initialState = {
+    user: {},
+    isAuth: false,
+    isLoading: false,
+    errors: null
+};
+
+export const authReducer = (state = initialState, {type, payload}) => {
+    switch (type) {
+        default:
+            return state;
+    }
+};
