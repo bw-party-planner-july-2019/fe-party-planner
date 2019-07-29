fbe7064a9cfe29a0dc2519d958a458ab1532693b
Donald Whitely <dswhitely1@gmail.com>
2019-07-28 22:42:41 -0400

    Auth Actions Finished, Ready for Testing

--- src/store/auth/reducers.js
+++ src/store/auth/reducers.js
@@ -1,13 +1,45 @@
+import {
+	LOGIN_START,
+	LOGIN_SUCCESS,
+	LOGIN_FAIL,
+	REGISTER_START,
+	REGISTER_SUCCESS,
+	REGISTER_FAIL,
+	LOGOUT,
+	WELCOME_BACK,
+} from './types';
+import jwt_decode from 'jwt-decode';
 const initialState = {
-    user: {},
-    isAuth: false,
-    isLoading: false,
-    errors: null
+	user      : {
+		username : '',
+		userID   : null,
+	},
+	isAuth    : false,
+	isLoading : false,
+	errors    : null,
 };
 
-export const authReducer = (state = initialState, {type, payload}) => {
-    switch (type) {
-        default:
-            return state;
-    }
+export const authReducer = (state = initialState, { type, payload }) => {
+	switch (type) {
+		case LOGIN_START:
+			return { ...state, isLoading: true, isAuth: false, errors: null };
+		case LOGIN_SUCCESS:
+			const { token, ...user } = payload;
+			return { ...state, isLoading: false, isAuth: true, user, errors: null };
+		case LOGIN_FAIL:
+			return { ...state, isLoading: false, isAuth: false, user: { username: '', userID: null }, errors: payload };
+		case REGISTER_START:
+			return { ...state, isLoading: true, isAuth: false, errors: null };
+		case REGISTER_SUCCESS:
+			return { ...state, isLoading: false, isAuth: false, errors: null };
+		case REGISTER_FAIL:
+			return { ...state, isLoading: false, isAuth: false, errors: payload };
+		case LOGOUT:
+			return initialState;
+		case WELCOME_BACK:
+			const { subject, username } = jwt_decode(payload);
+			return { ...state, isLoading: false, isAuth: true, user: { username, userID: subject }, errors: null };
+		default:
+			return state;
+	}
 };
--- src/store/auth/useAuthActions.jsx
+++ src/store/auth/useAuthActions.jsx
@@ -1,16 +1,47 @@
-import {useCallback} from 'react';
-import {useDispatch} from 'react-redux';
+import { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import { useLocalStorage } from '../../hooks/useLocalStorage';
+import { partyApi as axios } from '../../helpers/axiosConfig';
+import {
+	LOGIN_FAIL,
+	LOGIN_START,
+	LOGIN_SUCCESS,
+	LOGOUT,
+	REGISTER_FAIL,
+	REGISTER_START,
+	REGISTER_SUCCESS,
+	WELCOME_BACK,
+} from './types';
 
 export const useAuthActions = () => {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
+	const { getLocalStorage } = useLocalStorage('token');
+	const login = useCallback((credentials) => {
+		dispatch({ type: LOGIN_START });
+		axios.post('/auth/login', credentials).then((res) =>
+			dispatch({
+				type    : LOGIN_SUCCESS,
+				payload : res.data,
+			}).catch((err) => dispatch({ type: LOGIN_FAIL, payload: err.response.data })),
+		);
+	}, []);
 
-  const login = credentials => {};
+	const register = useCallback((newUser) => {
+		dispatch({ type: REGISTER_START });
+		axios
+			.post('/auth/register', newUser)
+			.then((res) => dispatch({ type: REGISTER_SUCCESS, payload: res.data }))
+			.catch((err) => dispatch({ type: REGISTER_FAIL, payload: REGISTER_FAIL }));
+	}, []);
 
-  const register = newUser => {};
+	const logout = useCallback(() => {
+		dispatch({ type: LOGOUT });
+	}, []);
 
-  const logout = () => {};
+	const welcomeBack = useCallback(() => {
+		const token = getLocalStorage();
+		dispatch({ type: WELCOME_BACK, payload: token });
+	}, []);
 
-  const welcomeBack = () => {};
-
-  return [login, register, logout, welcomeBack]
-}
+	return [ login, register, logout, welcomeBack ];
+};
