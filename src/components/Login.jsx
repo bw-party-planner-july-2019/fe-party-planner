1df575e9fd3bce67935c1f43d0dea1f1eaac97d9
connor mcnamee <connorjamesmcnamee@gmail.com>
2019-07-29 15:11:56 -0400

    getting somewhere with the login. not sure if I'm doing this right

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isRegister, setIsRegister] = useState(false);
+
+  const changeHandler = event => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    console.log(username);
+    console.log(password);
+  }
+
   return (
     <div className="Login">
-      <form>
+      <h1>{isRegister ? 'Register' : 'Login'}</h1>
+      <form onSubmit={event => handleSubmit(event)}>
         <label>
-        Username:
-          <input type="text" />
+         Username:
+          <input type="text" onChange={changeHandler}/>
           </label>
         <label>
           Password:
-          <input type="text" />
+          <input type="text" onChange={changeHandler}/>
         </label>
+        <button>{isRegister ? 'Register' : 'Login'}</button>
       </form>
     </div>
   );
