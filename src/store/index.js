458ddd67b546653bd0889f52f94f7e8686af3213
Donald Whitely <dswhitely1@gmail.com>
2019-07-29 23:10:42 -0400

    Finished  for the evening

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,21 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
-import {composeWithDevTools} from 'redux-devtools-extension';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import {authMiddleware} from "./middleware/authMiddleware";
+import { authMiddleware } from './middleware/authMiddleware';
 
-import {authReducer as auth} from './auth/reducers';
-import {eventsReducer as events} from "./events/reducers";
+import { authReducer as auth } from './auth/reducers';
+import { eventsReducer as party } from './party/reducers';
+import { shoppingReducer as shopping } from './shopping/reducers';
+import { todosReducer as todos } from './todos/reducers';
+const rootReducer = combineReducers({ auth, party, shopping, todos });
 
-const rootReducer = combineReducers({auth, events});
+const middleware =
+	process.env.NODE_ENV === 'development' ? [ thunk, logger, authMiddleware ] : [ thunk, authMiddleware ];
 
-const middleware = process.env.NODE_ENV === 'development' ? [thunk, logger, authMiddleware] : [thunk, authMiddleware];
-
-const enhancers = process.env.NODE_ENV === 'development' ? composeWithDevTools(applyMiddleware(...middleware)) : applyMiddleware(...middleware);
+const enhancers =
+	process.env.NODE_ENV === 'development'
+		? composeWithDevTools(applyMiddleware(...middleware))
+		: applyMiddleware(...middleware);
 
 export default createStore(rootReducer, enhancers);
