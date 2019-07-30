f4bc309c419c4a43140a39fdfeadd6f759c40ae7
Donald Whitely <dswhitely1@gmail.com>
2019-07-29 22:27:47 -0400

    Added PartyActions to Context

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import {useAuthActions} from '../store/auth/useAuthActions';
-import {ActionsProvider} from '../contexts/ActionsContext';
-import {Switch, Route} from 'react-router-dom';
+import { useAuthActions } from '../store/auth/useAuthActions';
+import { ActionsProvider } from '../contexts/ActionsContext';
+import { Switch, Route } from 'react-router-dom';
+import { usePartyActions } from '../store/party/usePartyActions';
 
 const App = () => {
-  const authActions = useAuthActions();
+	const authActions = useAuthActions();
+	const partyActions = usePartyActions();
 
-  return (
-      <ActionsProvider value={{authActions}}>
-        <Switch>
-          <Route path='/' render={() => <h1>Hello World</h1>}/>
-        </Switch>
-      </ActionsProvider>
-  );
+	return (
+		<ActionsProvider value={{ authActions, partyActions }}>
+			<Switch>
+				<Route path='/' render={() => <h1>Hello World</h1>} />
+			</Switch>
+		</ActionsProvider>
+	);
 };
 
 export default App;
