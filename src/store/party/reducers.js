fdb1ae770c904d4e0dc9dc123eca835f58a81d18
Donald Whitely <dswhitely1@gmail.com>
2019-07-30 14:03:25 -0400

    Updated Party Store

--- src/store/party/reducers.js
+++ src/store/party/reducers.js
@@ -1,12 +1,34 @@
+import {
+  GET_ALL_PARTIES_FAIL,
+  GET_ALL_PARTIES_START,
+  GET_ALL_PARTIES_SUCCESS,
+  GET_SINGLE_PARTY_FAIL,
+  GET_SINGLE_PARTY_START,
+  GET_SINGLE_PARTY_SUCCESS,
+} from './types';
+
 const initialState = {
-	party     : [],
-	isLoading : false,
-	errors    : null,
+  parties: [],
+  party: {},
+  isLoading: false,
+  errors: null,
 };
 
-export const eventsReducer = (state = initialState, { type, payload }) => {
-	switch (type) {
-		default:
-			return state;
-	}
+export const eventsReducer = (state = initialState, {type, payload}) => {
+  switch (type) {
+    case GET_ALL_PARTIES_START:
+      return {...state, isLoading: true, errors: null};
+    case GET_ALL_PARTIES_SUCCESS:
+      return {...state, isLoading: false, parties: payload, errors: null};
+    case GET_ALL_PARTIES_FAIL:
+      return {...state, isLoading: false, parties: [], errors: payload};
+    case GET_SINGLE_PARTY_START:
+      return {...state, isLoading: true, errors: null};
+    case GET_SINGLE_PARTY_SUCCESS:
+      return {...state, isLoading: false, errors: null, party: payload};
+    case GET_SINGLE_PARTY_FAIL:
+      return {...state, isLoading: false, errors: payload, party: {}};
+    default:
+      return state;
+  }
 };
