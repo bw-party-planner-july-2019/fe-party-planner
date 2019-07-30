abe26298c368beea1edede741fd32d7588768053
Donald Whitely <dswhitely1@gmail.com>
2019-07-29 22:23:52 -0400

    Party Store Defined

---
+++ src/store/party/reducers.js
@@ -0,0 +1,12 @@
+const initialState = {
+	party     : [],
+	isLoading : false,
+	errors    : null,
+};
+
+export const eventsReducer = (state = initialState, { type, payload }) => {
+	switch (type) {
+		default:
+			return state;
+	}
+};
