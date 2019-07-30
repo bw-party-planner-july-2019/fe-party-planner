bbf0de2365bb9d98ad9f7ab1a36ac3d95fdd7f12
Mimi Hoang <miminhoang1@gmail.com>
2019-07-30 10:24:32 -0400

    Dashboard creat

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,6 +12,7 @@ import Homepage from './homepage/Homepage';
 import party from '../imgs/party.jpg';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import {createMuiTheme, MuiThemeProvider} from '@material-ui/core/styles';
+import Dashboard from './Dashboard/Dashboard';
 
 const theme = createMuiTheme({
   palette: {
@@ -43,7 +44,7 @@ const App = () => {
             <Navigation/>
             <Switch>
               <Route path='/list' component={List}/>
-              <Route path='/dashboard' component={Form}/>
+              <Route path='/dashboard' component={Dashboard}/>
               <Route path='/register' render={props => <Login {...props} />}/>
               <Route path='/login' render={props => <Login {...props} />}/>
               <Route path='/' component={Homepage}/>
---
+++ src/components/Dashboard/Dashboard.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const data = [
+    {
+      "id": 1,
+      "party_name": "Ken B-day",
+      "n_of_guests": 50,
+      "date": "2019-10-07",
+      "theme": "80s",
+      "budget": 500,
+      "user_id": 1
+    },
+    {
+      "id": 2,
+      "party_name": "Office party",
+      "n_of_guests": 150,
+      "date": "2019-12-20",
+      "theme": "X-mas",
+      "budget": 2300,
+      "user_id": 2
+    },
+    {
+      "id": 3,
+      "party_name": "Halloween Party",
+      "n_of_guests": 90,
+      "date": "2019-10-31",
+      "theme": "Zombies",
+      "budget": 90,
+      "user_id": 3
+    }
+  ]
+ const Dashboard = () => <h1> Dashboard </h1>
+  export default Dashboard
