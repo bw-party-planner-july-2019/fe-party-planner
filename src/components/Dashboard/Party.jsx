b1962f3ca98a4a081985c8743a289691ba8b5bb1
Donald Whitely <dswhitely1@gmail.com>
2019-07-31 07:03:21 -0400

    Added User cannot edit or delete Parties that are not associated with their user_id

--- src/components/Dashboard/Party.jsx
+++ src/components/Dashboard/Party.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import data from './Dashboard';
-import { makeStyles } from '@material-ui/core/styles';
-import { shadows } from '@material-ui/system';
+import React, {useEffect, useState} from 'react';
+import {withRouter} from 'react-router-dom';
+import {makeStyles} from '@material-ui/core/styles';
 
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
@@ -10,14 +9,14 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import moment from 'moment';
+import {useSelector} from 'react-redux';
 
 const useStyles = makeStyles({
   card: {
     color: '#fff',
     background: '#B33771',
     border: 2,
-    marginLeft: 20,
-    marginRight: 20,
+
   },
 
   title: {
@@ -33,23 +32,28 @@ const useStyles = makeStyles({
   },
 });
 
-export default function SimpleCard(props) {
-    console.log(props)
+function Party(props) {
+  console.log(props);
   const classes = useStyles();
+  const [isSingle, setIsSingle] = useState(false);
+  const user_Id = useSelector(state => state.auth.user.userID);
+
+  useEffect(() => props.match.params.id && setIsSingle(true), []);
 
   return (
-      <Grid item xs>
-
+      <Grid item xs={12} sm={6} md={4} xl={3}>
         <Card className={classes.card}>
           <CardContent>
-            <Typography className={classes.title} color="textSecondary" gutterBottom>
+            <Typography className={classes.title} color="textSecondary"
+                        gutterBottom>
               {`Party Name: ${props.item.party_name}`}
             </Typography>
             <Typography className={classes.pos}>
               {`Number of Guests: ${props.item.n_of_guests}`}
             </Typography>
             <Typography className={classes.pos}>
-              {`Date: ${moment(props.item.date).format('MMMM Do YYYY, h:mm:ss a')}`}
+              {`Date: ${moment(props.item.date)
+                  .format('MMMM Do YYYY, h:mm:ss a')}`}
             </Typography>
             <Typography className={classes.pos}>
               {`Theme: ${props.item.theme}`}
@@ -58,12 +62,14 @@ export default function SimpleCard(props) {
               {`Budget: ${props.item.budget}`}
             </Typography>
           </CardContent>
-                 <CardActions>
-        <Button color="secondary" size="large">View</Button>
-        <Button color="secondary" size="large">Edit</Button>
-        <Button color="secondary" size="large">Delete</Button>
-      </CardActions>
+          <CardActions>
+            <Button color="secondary" size="large">View</Button>
+            {user_Id === props.item.user_id && <Button color="secondary" size="large">Edit</Button>}
+            {user_Id === props.item.user_id && <Button color="secondary" size="large">Delete</Button>}
+          </CardActions>
         </Card>
-     </Grid> 
+      </Grid>
   );
 }
+
+export default withRouter(Party);
