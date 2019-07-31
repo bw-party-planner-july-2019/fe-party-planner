d4f925a6c84d0cf5001ce5a894a4480b4fc76bb1
Donald Whitely <dswhitely1@gmail.com>
2019-07-31 08:48:28 -0400

    Added Add and Edit Finite States to Reducer

--- src/store/party/reducers.js
+++ src/store/party/reducers.js
@@ -1,10 +1,16 @@
 import {
+  ADD_PARTY_FAIL,
+  ADD_PARTY_START,
+  ADD_PARTY_SUCCESS,
   GET_ALL_PARTIES_FAIL,
   GET_ALL_PARTIES_START,
   GET_ALL_PARTIES_SUCCESS,
   GET_SINGLE_PARTY_FAIL,
   GET_SINGLE_PARTY_START,
   GET_SINGLE_PARTY_SUCCESS,
+  UPDATE_PARTY_FAIL,
+  UPDATE_PARTY_START,
+  UPDATE_PARTY_SUCCESS,
 } from './types';
 
 const initialState = {
@@ -27,6 +33,31 @@ export const eventsReducer = (state = initialState, {type, payload}) => {
     case GET_SINGLE_PARTY_SUCCESS:
       return {...state, isLoading: false, errors: null, party: payload};
     case GET_SINGLE_PARTY_FAIL:
+      return {...state, isLoading: false, errors: payload, party: {}};
+    case ADD_PARTY_START:
+      return {...state, isLoading: true, errors: null, party: {}};
+    case ADD_PARTY_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        errors: null,
+        party: {},
+        parties: {...state.parties, payload},
+      };
+    case ADD_PARTY_FAIL:
+      return {...state, isLoading: false, errors: payload, party: {}};
+    case UPDATE_PARTY_START:
+      return {...state, isLoading: true, errors: null, party: {}};
+    case UPDATE_PARTY_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        errors: null,
+        party: {},
+        parties: state.parties.map(
+            party => party.id === payload.id ? payload : party),
+      };
+    case UPDATE_PARTY_FAIL:
       return {...state, isLoading: false, errors: payload, party: {}};
     default:
       return state;
