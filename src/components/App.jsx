84076ba67bbe573122ee91f123582f28d9c567be
Donald Whitely <dswhitely1@gmail.com>
2019-07-31 06:43:45 -0400

    Removed Unused Imports, Changed Name of AddPartyForm component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,24 +3,20 @@ import styled from 'styled-components';
 import Login from './Login';
 import {useAuthActions} from '../store/auth/useAuthActions';
 import {ActionsProvider} from '../contexts/ActionsContext';
-import {Switch, Route} from 'react-router-dom';
-import Container from '@material-ui/core/Container'
-import Form from './AddPartyForm';
+import {Route, Switch} from 'react-router-dom';
+import Container from '@material-ui/core/Container';
+import AddPartyForm from './Dashboard/AddPartyForm';
 import List from './list/List';
 import {usePartyActions} from '../store/party/usePartyActions';
 import Navigation from './navigation/Navigation';
-import Homepage from './homepage/Homepage';
 import party from '../imgs/party.jpg';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import {createMuiTheme, MuiThemeProvider} from '@material-ui/core/styles';
 import Dashboard from './Dashboard/Dashboard';
-import Party from './Dashboard/Party';
 import PrivateRoute from './auth/PrivateRoute';
 
 const theme = createMuiTheme({
-  typography: {
-
-  },
+  typography: {},
   palette: {
     primary: {main: '#B33771'},
     secondary: {main: '#FEA47F'},
@@ -39,15 +35,17 @@ const BackgroundImage = styled.div`
   background-size: cover;
 `;
 
-const list = [{ id: 1, party_id: 1, item: 'balloons', purchased: false, price: 5 },
-  { id: 2, party_id: 1, item: 'drinks', purchased: false, price: 55 },
-  { id: 3, party_id: 1, item: 'cake', purchased: false, price: 20 },
-  { id: 4, party_id: 2, item: 'juice', purchased: false, price: 5 },
-  { id: 5, party_id: 2, item: 'cups', purchased: false, price: 10 },
-  { id: 6, party_id: 2, item: 'cookies', purchased: false, price: 15 },
-  { id: 7, party_id: 3, item: 'decorations', purchased: false, price: 25 },
-  { id: 8, party_id: 3, item: 'candy', purchased: false, price: 20 },
-  { id: 9, party_id: 3, item: 'music', purchased: false, price: 10 }];
+const list = [
+  {id: 1, party_id: 1, item: 'balloons', purchased: false, price: 5},
+  {id: 2, party_id: 1, item: 'drinks', purchased: false, price: 55},
+  {id: 3, party_id: 1, item: 'cake', purchased: false, price: 20},
+  {id: 4, party_id: 2, item: 'juice', purchased: false, price: 5},
+  {id: 5, party_id: 2, item: 'cups', purchased: false, price: 10},
+  {id: 6, party_id: 2, item: 'cookies', purchased: false, price: 15},
+  {id: 7, party_id: 3, item: 'decorations', purchased: false, price: 25},
+  {id: 8, party_id: 3, item: 'candy', purchased: false, price: 20},
+  {id: 9, party_id: 3, item: 'music', purchased: false, price: 10},
+];
 
 const App = () => {
   const authActions = useAuthActions();
@@ -60,15 +58,14 @@ const App = () => {
           <BackgroundImage>
             <Navigation/>
             <Container fluid>
-            <Switch>
-             <PrivateRoute path='/dashboard/add-party' component={Form} />
-
-              <Route path='/list' render={()=> <List party_id={1} list={list} />}/>
-
-              <PrivateRoute path='/dashboard' component={Dashboard}/>
-              <Route path='/register' render={props => <Login {...props} />}/>
-              <Route path='/' render={props => <Login {...props} />}/>
-            </Switch>
+              <Switch> <PrivateRoute path='/dashboard/add-party'
+                                     component={AddPartyForm}/>
+                <Route path='/list'
+                       render={() => <List party_id={1} list={list}/>}/>
+                <PrivateRoute path='/dashboard' component={Dashboard}/>
+                <Route path='/register' render={props => <Login {...props} />}/>
+                <Route path='/' render={props => <Login {...props} />}/>
+              </Switch>
             </Container>
           </BackgroundImage>
         </ActionsProvider>
