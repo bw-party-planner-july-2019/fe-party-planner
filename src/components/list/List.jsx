5492f66cf35ad9b80be69b20daf2a763daf8d38b
Donald Whitely <dswhitely1@gmail.com>
2019-07-31 08:13:04 -0400

    Fixed List Component to View on Single View

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -36,9 +36,20 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
-export default function DisplayList({party_id, list}) {
+export default function DisplayList() {
+    const list = [
+        {id: 1, party_id: 1, item: 'balloons', purchased: false, price: 5},
+        {id: 2, party_id: 1, item: 'drinks', purchased: false, price: 55},
+        {id: 3, party_id: 1, item: 'cake', purchased: false, price: 20},
+        {id: 4, party_id: 2, item: 'juice', purchased: false, price: 5},
+        {id: 5, party_id: 2, item: 'cups', purchased: false, price: 10},
+        {id: 6, party_id: 2, item: 'cookies', purchased: false, price: 15},
+        {id: 7, party_id: 3, item: 'decorations', purchased: false, price: 25},
+        {id: 8, party_id: 3, item: 'candy', purchased: false, price: 20},
+        {id: 9, party_id: 3, item: 'music', purchased: false, price: 10},
+    ];
     const classes = useStyles();
-    const listOfItems = list.filter(item => item.party_id = party_id);
+    const listOfItems = list.filter(item => item.party_id === list.party_id);
     const [checked, setChecked] = React.useState([0]);
 
     const handleToggle = value => () => {
@@ -56,7 +67,7 @@ export default function DisplayList({party_id, list}) {
 
     return (
         <div>
-            <h1>Dashboard</h1>
+            <h1>List Component</h1>
             <Card className={classes.card}>
                 <List className={classes.list}>
                     {console.log("checked items ", checked)}
