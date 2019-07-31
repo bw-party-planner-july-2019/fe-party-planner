225ba9d7492770ab7fca3e9946376973b24410af
Donald Whitely <dswhitely1@gmail.com>
2019-07-31 09:12:06 -0400

    Full CRUD Operations working for Party

--- src/store/party/reducers.js
+++ src/store/party/reducers.js
@@ -2,6 +2,9 @@ import {
   ADD_PARTY_FAIL,
   ADD_PARTY_START,
   ADD_PARTY_SUCCESS,
+  DELETE_PARTY_FAIL,
+  DELETE_PARTY_START,
+  DELETE_PARTY_SUCCESS,
   GET_ALL_PARTIES_FAIL,
   GET_ALL_PARTIES_START,
   GET_ALL_PARTIES_SUCCESS,
@@ -58,6 +61,18 @@ export const eventsReducer = (state = initialState, {type, payload}) => {
             party => party.id === payload.id ? payload : party),
       };
     case UPDATE_PARTY_FAIL:
+      return {...state, isLoading: false, errors: payload, party: {}};
+    case DELETE_PARTY_START:
+      return {...state, isLoading: true, errors: null, party: {}};
+    case DELETE_PARTY_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        errors: null,
+        party: {},
+        parties: state.parties.filter(party => party.id !== payload),
+      };
+    case DELETE_PARTY_FAIL:
       return {...state, isLoading: false, errors: payload, party: {}};
     default:
       return state;
