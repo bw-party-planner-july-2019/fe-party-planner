92a7b9228983c407d25fd0b735eb932b278172ea
Donald Whitely <dswhitely1@gmail.com>
2019-07-31 10:45:34 -0400

    Reformatted Code

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,24 +1,21 @@
-import React, {useEffect, useContext} from 'react';
+import React, {useContext, useEffect} from 'react';
 import Party from './Party';
 import {useSelector} from 'react-redux';
 import {ActionsContext} from '../../contexts/ActionsContext';
 import Grid from '@material-ui/core/Grid';
 
- const Dashboard = () => {
-   const parties = useSelector(state=>state.party.parties);
-   const {partyActions: {fetchParties}} = useContext(ActionsContext);
+const Dashboard = () => {
+  const parties = useSelector(state => state.party.parties);
+  const {partyActions: {fetchParties}} = useContext(ActionsContext);
 
-   useEffect(()=> fetchParties(),[]);
-
-   return(
-     <div>
-          
-       
-         <h1>Dashboard</h1>
-         <Grid container spacing={2}>
-            {parties.map((item, i)=> <Party item={item} key={i} />)}
-         </Grid>
-     </div>
-    )
-}
-  export default Dashboard
+  useEffect(() => fetchParties(), []);
+  return (
+      <div>
+        <h1>Dashboard</h1>
+        <Grid container spacing={2}>
+          {parties.map((item, i) => <Party item={item} key={i}/>)}
+        </Grid>
+      </div>
+  );
+};
+export default Dashboard;
