38a78ef3d174890162af348f67792364db6146c5
Donald Whitely <dswhitely1@gmail.com>
2019-07-31 06:43:18 -0400

    Relocated File to Dashboard Folder

--- src/components/Dashboard/AddPartyForm.jsx
+++ src/components/Dashboard/AddPartyForm.jsx
@@ -3,7 +3,7 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import styled from 'styled-components';
 import {useSelector} from 'react-redux';
-import {ActionsContext} from '../contexts/ActionsContext';
+import {ActionsContext} from '../../contexts/ActionsContext';
 
 const TempStyle = styled.div`
   width: 100%;
