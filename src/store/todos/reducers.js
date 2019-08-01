3290fba92de03df6611fb73dce87dfe0c9b92a77
Donald Whitely <dswhitely1@gmail.com>
2019-08-01 08:03:38 -0400

    Added LOGOUT cases to Reducers

--- src/store/todos/reducers.js
+++ src/store/todos/reducers.js
@@ -1,8 +1,16 @@
-const initialState = [];
+import { LOGOUT } from '../auth/types';
+
+const initialState = {
+  list: [],
+  isLoading: false,
+  errors: null,
+};
 
 export const todosReducer = (state = initialState, { type, payload }) => {
-	switch (type) {
-		default:
-			return state;
-	}
+  switch (type) {
+    case LOGOUT:
+      return initialState;
+    default:
+      return state;
+  }
 };
